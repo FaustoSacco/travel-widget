477ea59bf2e3c42c2c56cf977995c1d91d10b505
Fausto Sacco <faustosacco@Faustos-iMac.local>
2023-06-04 19:18:32 +0100

    finalised styling and components

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
+
 
 app.get('/flights', (req, res) => {
     const options = {
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,14 @@
+import TableHead from './TableHead'
+import TableBody from './TableBody'
+
+
 const Table = () => {
     return (
-  <></>
+      <table>
+        <TableHead/>
+
+        <TableBody/>
+      </table>
     );
   }
   
